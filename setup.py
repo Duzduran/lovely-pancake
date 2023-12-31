6d7146250af782b835bec19d4da6867a38e9a2e2
Duzduran <ahmetduzduran@gmail.com>
2023-12-31 03:19:02 +0100

    new release

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', encoding='utf-8') as f:
 setup(
 
     name='lovely-pancake',
-    version='1.0.5',
+    version='1.0.6',
     author='Ahmet Duzduran',
     description='My lovely python package',
     packages=['lovelypancake'],
